6de4406ced5fd6f5d53ec0bd364797fd9a7913dd
Daniel de R <derepentigny.daniel@gmail.com>
2019-09-08 21:02:17 -0400

    running in CF

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask
 import os
+from cfenv import AppEnv
 
 app = Flask(__name__)
 
 cf_port = os.getenv("PORT")
 
+env = AppEnv()
+mlab = env.get_service(label='mlab')
+
+
 @app.route('/')
 def hello_world():
-    return f"I'm running! On port {cf_port}"
+    return f"I'm running! On port {cf_port}, {mlab.credentials['uri']}"
 
 if __name__ == '__main__':
     if cf_port is None:
